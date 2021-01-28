2bc057d29a4201559313f6f02935d0fd488ef522
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2021-01-28 14:00:14 -0600

    added getPosts and getPostById GET calls

---
+++ src/components/posts/PostsProvider.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react"
+
+export const AnimalContext = React.createContext()
+
+export const PostProvider = (props) => {
+    const [posts, setPosts] = useState([])
+
+    const getPosts = () => {
+        return fetch("http://localhost:8088/posts")
+        .then(res => res.json())
+           .then(setPosts)
+    }
+
+    const getPostById = (id) => {
+       return fetch(`http://localhost:8088/posts/${ id }`)
+           .then(res => res.json())
+   }
+   return (
+    <PostContext.Provider value={{
+        posts, getPosts, getPostById
+    }}>
+        {props.children}
+    </PostContext.Provider>
+)
+}
