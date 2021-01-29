af58d7661cb7f02c9dfedd399ff06200e93b7f48
Suganya <suganprakash0621@gmail.com>
2021-01-29 09:11:23 -0600

    got o/p for Category ticket

--- src/components/category/CategoryDashBoard.js
+++ src/components/category/CategoryDashBoard.js
@@ -14,17 +14,12 @@ useEffect(() =>{
     getCategory()
  },[])
 
- //  For finding the Category name
-//  useEffect(() => {
-//     const findCategory = category.find(a => a.id === parseInt(props.match.history.categoryId))|| {}
-//     setCategory(findCategory)
-//         },[Category])
 
 
 console.log(category)
 return (
     <>
-    <section className="users">
+   
     <h2>Categories</h2>
     {
        category.map(C =>{
@@ -34,15 +29,6 @@ return (
         </Link>
        }) 
     }
-
-<section className="button1">
-    <button  variant="primary" size="sm" onClick={() => 
-        props.history.push(`/categories/edit/${Category.id}`)}>
-    Edit Category
-   </button> 
-   </section>
-   </section>
-
 <section className="categories">
 <h3 className="kiduser__name">Create a New Category:</h3>
 
