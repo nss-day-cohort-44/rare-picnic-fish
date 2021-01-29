7831a9455bb34a63a750854e335008c712d8d5df
Jaron-Lane <jaronwohlwend@icloud.com>
2021-01-29 16:56:35 -0600

    Building out Comments and CommentList

--- src/components/comments/CommentList.js
+++ src/components/comments/CommentList.js
@@ -1,34 +1,28 @@
 import React, { useContext, useEffect } from "react"
-import { Link } from "react-router-dom"
 import { CommentContext } from "./CommentsProvider"
+import Comment from "./Comment"
 
 
 export const CommentList = (props) => {
-    const { comments, getComments } = useContext(HabitContext)
+    const { comments, getComments } = useContext(CommentContext)
 
     useEffect(() => {
         getComments()
     }, [])
 
-    // sets userId equal to the app_user_id from local storage. Also, converting it to an int from a string
-    const userId = parseInt(localStorage.getItem('rare_user_id'))
-
     return (
         <>
-            <div className="todays__habits">
-                <h1>Library</h1>
-            </div>
-            <div className="allHabit__container">
-                
-                    {
-                        // filter over all the habits and get the ones where the habit's userId matches the user's id
-                        habits.filter(habit => habit.userId === userId).map(habit => {
-                            return <Link className="allHabit__cards" key={habit.id} to={`/${habit.id}`} props={props}>
-                                <h2>{habit.name}</h2>
-                            </Link>
-                        }
-                        )}
-                
+            <h1>Comments</h1>
+
+            {/* <button onClick={() => history.push("/animals/create")}>
+                Make Reservation
+            </button> */}
+            <div className="comments">
+                {
+                    comments.map(comment => {
+                        return <Comment key={comment.id} comment={comment} />
+                    })
+                }
             </div>
         </>
     )
