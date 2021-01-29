bda961dda52813cff909555a15d12f27926b6848
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2021-01-29 09:01:40 -0600

    added PostForm function and started adding input fields

--- src/components/posts/PostForm.js
+++ src/components/posts/PostForm.js
@@ -1,2 +1,51 @@
-import React, { useContext, useEffect, useState } from "react";
-import { PostContext } from "./PostsProvider"
+import React, { useContext, useEffect, useState, useRef } from "react";
+import { PostContext } from "./PostProvider"
+
+export const PostForm = (props) => {
+    const { posts, getPosts, getPost, addPost} = useContext(PostContext)
+
+    const name = useRef(null)
+
+    useEffect(() => {
+        getPosts()
+    }, [])
+    
+    const addNewPost = () => {
+        addPost({
+            userId,
+            categoryId, 
+            title,
+            publicationDate,
+            imageUrl,
+            content,
+            approved
+        })
+        .then(() => props.history.push("/posts"))
+    }
+    return (
+        <form className="PostForm">
+            <h2 className="PostForm__title">Title</h2>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="Post-Title">Post Title: </label>
+                    <input type="text" id="Post-Title" ref={title} required autoFocus className="form-control" placeholder="Post Title" />
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="Post-Content">Post Content: </label>
+                    <input type="text" id="Post-Content" ref={content} required autoFocus className="form-control" placeholder="Post Content" />
+                </div>
+            </fieldset>
+           
+            <button type="submit"
+                onClick={evt => {
+                    evt.preventDefault() // Prevent browser from submitting the form
+                    constructNewPost()
+                }}
+                className="btn btn-primary">
+                Save Post
+            </button>
+        </form>
+    )
+}
