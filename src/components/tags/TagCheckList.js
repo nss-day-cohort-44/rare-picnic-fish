6b37cc800f535a660cd93464473a73c6aa2b9b7d
Leo Rondeau <christopherleorondeau@gmail.com>
2021-02-03 14:44:14 -0600

    added functionality for tag checklist

--- src/components/tags/TagCheckList.js
+++ src/components/tags/TagCheckList.js
@@ -1,15 +1,18 @@
 import React, { useState, useEffect, useRef, useContext } from "react";
 import { TagContext } from './TagProvider'
 import { Multiselect } from 'multiselect-react-dropdown'
+import { PostContext } from "../posts/PostProvider";
+
 
 
 
 export const TagCheckList = (props) => {
     const { tags, getTags, addPostTag } = useContext(TagContext)
+    const {posts, getPosts} = useContext(PostContext)
     const [selectedTags, setSelectedTags] = useState(tags)
-    const postId = parseInt(props.match.params.postId)
+    const propPostId = parseInt(props.match.params.postId)
 
-
+    // console.log("props", props)
     const handleControlledInputChange = (selectedTags) => {
 
         // Adding key value pairs to the object through each from input
@@ -17,17 +20,18 @@ export const TagCheckList = (props) => {
         //    Without this only the last key value pair entered will send
         const newPostTag = selectedTags.map(st => {
             const newObject = Object.assign({}, st);
-            newObject.post_id = postId
+            newObject.postId = propPostId
             return newObject
         })
         // newEvent[e.target.name] = e.target.value
         setSelectedTags(newPostTag)
     }
-    console.log("selectedTags", selectedTags)
+    // console.log("selectedTags", selectedTags)
 
     useEffect(() => {
 
         getTags()
+        .then(getPosts)
 
     }, [])
 
@@ -35,22 +39,33 @@ export const TagCheckList = (props) => {
 
     }, [selectedTags])
 
+    selectedTags.map(st => delete st.label)
+
+
+    const activeUserId = parseInt(localStorage.getItem("rare_user_id"))
+    
     const constructPostTags = () => addPostTag(selectedTags)
+    // const activeUser = posts.map(p => {p.user_id === activeUserId)
+    
+    // if (activeUser.id === localStorage.getItem("rare_user_id")) {
 
-    return (
-        <form>
-            <Multiselect defaultValue={[]} name="tags" options={tags} displayValue="label" className="basic-multi-select"
-                classNamePrefix="select" placeholder="Select Tags" onSelect={handleControlledInputChange} />
+        return (
+            <form>
+                <Multiselect defaultValue={[]} name="tags" options={tags} displayValue="label" className="basic-multi-select"
+                    classNamePrefix="select" placeholder="Select Tags" onSelect={handleControlledInputChange} />
 
-            <button type="submit" bsPrefix="form-button"
-                onClick={evt => {
-                    evt.preventDefault()
-                    constructPostTags()
-                }}
-                className="btn btn-primary">
-                Save Tags
+                <button type="submit"
+                    onClick={evt => {
+                        evt.preventDefault()
+                        constructPostTags()
+                    }}
+                    className="btn btn-primary">
+                    Save Tags
             </button>
-        </form>
-    )
-}
-
+            </form>
+        )
+    }
+//     else {
+//         return null
+//     }
+// }
